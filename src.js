fafd15e63dcda53b1a525819a5231a9417a65289
Brendan C <30474072+brendan-c@users.noreply.github.com>
2019-11-13 21:16:39 -0500

    Update src.js

--- src.js
+++ src.js
@@ -1,79 +1,50 @@
 ;(function() {
-    const vendors = {
-        ecigexpress: {
-          cart: 'https://ecigexpress.com/cart',
-          flavor: 'h5.product-name a'
-        },
-        bullcity: {
-          cart: 'https://www.bullcityflavors.com/cart.php',
-          flavor: '.cart-item h4 a'
-        },
-        chefsflavours: {
-          cart: 'https://chefsflavours.co.uk/cart.php',
-          flavor: '.cart-item h4 a'
-        },
-        nicriver: {
-          cart: 'https://riversupplyco.com/cart',
-          flavor: 'h5.product-title a'
-        },
-        flavourwala: {
-          cart: 'https://www.flavourwala.com/cart/',
-          flavor: 'td.product-name a'
-        }
+  const vendors = {
+    ecigexpress: {
+      cart: 'https://ecigexpress.com/cart',
+      flavor: 'h5.product-name a'
+    },
+    bullcity: {
+      cart: 'https://www.bullcityflavors.com/cart.php',
+      flavor: '.cart-item h4 a'
+    },
+    chefsflavours: {
+      cart: 'https://chefsflavours.co.uk/cart.php',
+      flavor: '.cart-item h4 a'
+    },
+    nicriver: {
+      cart: 'https://riversupplyco.com/cart',
+      flavor: 'h5.product-title a'
+    },
+    flavourwala: {
+      cart: 'https://www.flavourwala.com/cart/',
+      flavor: 'td.product-name a'
     }
-  
-    function getFlavors(vendor) {
-      function openForm() {
-        document.querySelector('.choose-vendor-outer').style.display = 'block'
-      }
+  }
 
-      const vendor = Object.entries(vendors)
-        .map(v => v[0])
-        .filter(e => document.domain.includes(e))[0]
+  function openForm() {
+    document.querySelector('.choose-vendor-outer').style.display = 'block'
+  }
 
-      if (!vendor) {
-        openForm()
-      }
-      function goToCart() {
-        const vendor = document.getElementById('vendors').value
-        if (!vendor.length) {
-          alert('Choose a vendor from the dropdown menu')
-          return
-        }
-        console.log(vendors.vendor)
-        window.open(vendors[vendor].cart, '_blank')
-      }
+  const vendor = Object.entries(vendors)
+    .map(v => v[0])
+    .filter(e => document.domain.includes(e))[0]
 
-      function closeVendorForm() {
-        document.querySelector('.choose-vendor-outer').style = 'display:none;'
-      }
+  if (!vendor) {
+    openForm()
+  }
   
-      if (!document.querySelectorAll(vendor.flavor).length) {
-        alert('You have no flavors in your cart to copy.')
-        return
-      }
-  
-      const flavors =
-        '```\n' +
-        Array.from(document.querySelectorAll(vendor.flavor))
-          .map(f => f.childNodes[0].textContent.trim())
-          .sort()
-          .join('\n')
-          .concat('\n```')
-
-      const copyToClipboard = str => {
-        const el = document.createElement('textarea')
-        el.value = str
-        document.body.appendChild(el)
-        el.select()
-        document.execCommand('copy')
-        document.body.removeChild(el)
-      }
-  
-      copyToClipboard(flavors)
-      alert('flavors have been copied to, go paste them somewhere!')
+  function goToCart() {
+    const vendor = document.getElementById('vendors').value
+    if (!vendor.length) {
+      alert('Choose a vendor from the dropdown menu')
+      return
     }
-  
-    // getFlavors(vendors.bullcity)
-  })()
-  
+    console.log(vendors.vendor)
+    window.open(vendors[vendor].cart, '_blank')
+  }
+
+  function closeVendorForm() {
+    document.querySelector('.choose-vendor-outer').style = 'display:none;'
+  }
+})()
