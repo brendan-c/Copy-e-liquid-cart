511f4a8c84952207cdd93ef50ccf16eee1b6a12c
Brendan C <30474072+brendan-c@users.noreply.github.com>
2019-11-16 17:06:19 -0500

    Update src.js

--- src.js
+++ src.js
@@ -1,75 +1,54 @@
-;
-(function() {
-    const vendors = {
-        ecigexpress: {
-            cart: 'https://ecigexpress.com/cart',
-            flavor: 'h5.product-name a'
-        },
-        bullcity: {
-            cart: 'https://www.bullcityflavors.com/cart.php',
-            flavor: '.cart-item h4 a'
-        },
-        chefsflavours: {
-            cart: 'https://chefsflavours.co.uk/cart.php',
-            flavor: '.cart-item h4 a'
-        },
-        nicriver: {
-            cart: 'https://riversupplyco.com/cart',
-            flavor: 'h5.product-title a'
-        },
-        flavourwala: {
-            cart: 'https://www.flavourwala.com/cart/',
-            flavor: 'td.product-name a'
-        }
-    }
-    const v = Object.entries(vendors)
+
+  const vendorsJSON =
+    "https://raw.githubusercontent.com/brendan-c/Copy-e-liquid-cart/master/vendors.json";
+
+  fetch(vendorsJSON)
+    .then(res => res.text())
+    .then(str => {
+      const vendors = JSON.parse(str);
+      const v = Object.entries(vendors)
         .map(v => v[0])
-        .filter(e => document.domain.includes(e))
+        .filter(e => document.domain.includes(e));
 
-    if (!v.length) {
-        alert('You must go to your cart first.')
-        return
-    }
-    const vendor = vendors[v[0]]
-    console.log(vendor)
+      if (!v.length) {
+        alert("You must go to your cart first.");
+        return;
+      }
 
-    function getFlavors() {
-        if (!window.location.href.includes(vendor.cart)) {
-            if (
-                window.confirm(
-                    'You must go to your cart first. After pressing OK, paste the script again.'
-                )
-            ) {
-                window.location.replace(vendor.cart)
-                return
-            }
+      const vendor = vendors[v[0]];
+
+      if (!window.location.href.includes(vendor.cart)) {
+        if (
+          window.confirm(
+            "You must go to your cart first. After pressing OK, click the bookmark again"
+          )
+        ) {
+          window.location.replace(vendor.cart);
+          return;
         }
+      }
 
-        if (!document.querySelectorAll(vendor.flavor).length) {
-            alert('You have no flavors in your cart to copy.')
-            return
-        }
+      const flavorElements = document.querySelectorAll(vendor.flavor);
 
-        const flavors =
-            '```\n' +
-            Array.from(document.querySelectorAll(vendor.flavor))
-            .map(f => f.childNodes[0].textContent.trim())
-            .sort()
-            .join('\n')
-            .concat('\n```')
+      if (!flavorElements.length) {
+        alert("You have no flavors in your cart to copy.");
+        return;
+      }
 
-        const copyToClipboard = str => {
-            const el = document.createElement('textarea')
-            el.value = str
-            document.body.appendChild(el)
-            el.select()
-            document.execCommand('copy')
-            document.body.removeChild(el)
-        }
+      const flavors =
+        "```\n" +
+        Array.from(flavorElements)
+          .map(f => f.childNodes[0].textContent.trim())
+          .sort()
+          .join("\n")
+          .concat("\n```");
 
-        copyToClipboard(flavors)
-        alert('flavors have been copied to, go paste them somewhere!')
-    }
+      const el = document.createElement("textarea");
+      el.value = flavors;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand("copy");
+      document.body.removeChild(el);
 
-    getFlavors(vendor)
-})()
+      alert("flavors have been copied to, go paste them somewhere!");
+    });
